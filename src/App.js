af5d9a7e0318a0374a406476683b1e4fb9603170
Bunks <busk_1@yahoo.com>
2021-03-06 15:58:07 -0500

    added the home route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <main className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/home" component={Home} />
+      </main>
+    </Router>
   );
 }
 
