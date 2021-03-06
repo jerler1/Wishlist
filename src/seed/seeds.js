6985f988ccc3f9b2a91a106f2c2f4ad2f027a748
Bunks <busk_1@yahoo.com>
2021-03-06 16:50:53 -0500

    added seeds

---
+++ src/seed/seeds.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    company: "Pathrise",
+    jobTitle: "Web Developer",
+    thumbnail: require("./images/pathrise.png").default,
+  },
+  {
+    company: "Airbnb",
+    jobTitle: "Junior Developer",
+    thumbnail: require("./images/airbnb.png").default,
+  },
+  {
+    company: "Google",
+    jobTitle: "Intern",
+    thumbnail: require("./images/google.png").default,
+  },
+  {
+    company: "Facebook",
+    jobTitle: "React Developer",
+    thumbnail: require("./images/facebook.png").default,
+  },
+
+];
