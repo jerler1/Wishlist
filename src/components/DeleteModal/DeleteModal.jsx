9b7fc14451b9b5b3d15b5727545a156f92dcbc9d
Bunks <busk_1@yahoo.com>
2021-03-07 20:06:41 -0500

    added a button to the delete modal for modal closure

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -7,17 +7,30 @@ const DeleteModal = (props) => {
       <div className={props.deleteModalActive ? "modal is-active" : "modal"}>
         <div className="modal-background"></div>
         <div
-          className="modal-content has-background-white p-6"
+          className="modal-content has-background-white m-6 is-relative"
           id="deleteModal"
         >
           <div className="container">
+            <button
+              className="delete is-medium exitButton"
+              onClick={props.toggleDeleteModal}
+            ></button>
             <p className="has-text-centered is-size-2 has-text-weight-bold">
-              Delete Job
+              Delete a job
             </p>
+            <hr/>
             <p className="my-2">Are you sure you want to delete this job?</p>
 
-            <button onClick={props.deleteCard} className="button m-2 has-background-danger-dark has-text-white">Delete</button>
-            <button onClick={props.toggleDeleteModal} className="button m-2 has-background-link has-text-white">
+            <button
+              onClick={props.deleteCard}
+              className="button m-2 has-background-danger-dark has-text-white"
+            >
+              Delete
+            </button>
+            <button
+              onClick={props.toggleDeleteModal}
+              className="button m-2 has-background-link has-text-white"
+            >
               Cancel
             </button>
           </div>
